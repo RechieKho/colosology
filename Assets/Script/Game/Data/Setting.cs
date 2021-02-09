ef5c0a5d43906e3eb5d57c10e9a84ede3d248967
DeeHuman <deeamezinapple@gmail.com>
2021-02-09 00:48:34 -0800

    Add asset from Kenney's; Add New Scene (Menu); Add new Font (pixel one); Add new Class (Setting, GameManager);

---
+++ Assets/Script/Game/Data/Setting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BayatGames.SaveGameFree;
+
+namespace Game.Data
+{
+    public static class Setting
+    {
+        private static float _mainVolume; 
+        private static float _effectVolume;
+        private static float _bgmVolume;
+
+        private static float _detail; 
+
+        #region Method
+        // static methods
+        public static void LoadData() // load data from saved file
+        {
+            _mainVolume = SaveGame.Load<float>("_mainVolume");
+            _effectVolume = SaveGame.Load<float>("_effectVolume");
+            _bgmVolume = SaveGame.Load<float>("_bgmVolume");
+
+            _detail = SaveGame.Load<float>("_detail");
+        }
+
+        public static void SaveData() // save data to saved file
+        {
+            SaveGame.Save<float>("_mainVolume", _mainVolume);
+            SaveGame.Save<float>("_effectVolume", _effectVolume);
+            SaveGame.Save<float>("_bgmVolume", _bgmVolume);
+
+            SaveGame.Save<float>("_detail", _detail);
+        }
+        #endregion
+
+        #region Properties
+        public static float MainVolume { get { return _mainVolume; } set { _mainVolume = value; } }
+        public static float EffectVolume { get { return _effectVolume; } set { _effectVolume = value; } }
+        public static float BgmVolume { get { return _bgmVolume; } set { _bgmVolume = value; } }
+        
+        public static float Detail { get { return _detail; } set { _detail = value; } }
+        #endregion
+    }
+}
---
+++ Assets/Script/Game/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Variable
+        private static GameManager _singleton;
+        #endregion
+
+        #region Method
+        #region MonoBehaviour
+        // Start is called before the first frame update
+        void Start()
+        {
+            #region Create Singleton
+            if(_singleton == null)
+            {
+                // assuming script is attach on some unknown object
+                // create singleton
+                InitOnce();
+                // remove itself from gameObject
+                Destroy(this);
+            }
+            #endregion
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+
+        // singleton
+        private static void InitOnce()
+        {
+            if(_singleton == null)
+            {
+                _singleton = new GameObject("GameManager").AddComponent<GameManager>();
+                DontDestroyOnLoad(_singleton);
+            }
+        }
+
+        #endregion
+    }
+}
